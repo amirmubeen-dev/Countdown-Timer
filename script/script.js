aa14d05dfbfd641bcfc040920c20dd01d19c8f02
Amir Mubeen <amirmubeen.mw@gmail.com>
2025-07-27 11:52:06 +0500

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,21 @@
+const enddate="15 July 2025 1:00 PM"
+
+document.getElementById("enddate").innerHTML=enddate;
+const inputs= document.querySelectorAll("input")
+
+function clock(){
+    const end = new Date(enddate);
+    const now = new Date()
+
+    const diff= (end-now)/1000 //seconds
+    if (diff<0) {
+        return;
+    }
+    inputs[0].value=Math.floor(diff / 3600 / 24);
+    inputs[1].value=Math.floor(diff / 3600) % 24;
+    inputs[2].value=Math.floor(diff / 60 % 60);
+    inputs[3].value=Math.floor(diff) % 60;
+}
+setInterval(() => {
+    clock()
+}, 1000);
